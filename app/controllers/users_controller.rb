141214d3e973e037889987d08dab9610ddce8a4e
Leo <lwang2six@gmail.com>
2011-05-05 16:39:18 -0700

    added auto login after creating a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,6 +44,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      session[:user_id] = @user.id
       redirect_to(user_path(@user), :notice => 'User successfully added.')
     else
       render :action => 'new'
